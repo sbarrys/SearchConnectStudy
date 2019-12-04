8428c229990ef09d287d8feddc04fc253d654be3
jeky22 <qhzh33@gmail.com>
2019-12-04 14:31:20 +0900

    notice.vue 디자인 수정

--- frontend/src/views/Notice.vue
+++ frontend/src/views/Notice.vue
@@ -1,111 +1,92 @@
 
 <template>
-       <div class ="outer">
-              <div class="inner">
-                     <table class="notice">
-                            <tr>
-                                   <td>Type</td>
-                                   <td>Member</td>
-                                   <td>Writer</td>&nbsp;&nbsp;&nbsp;
-                                   <td>Title</td>&nbsp;&nbsp;&nbsp;&nbsp;
-                                   <td>Date</td>
-                            </tr>
+  <div class="container-fluid text-center" >
+    <b-table-simple hover class="table small table-hover">
+      <b-thead>
+        <b-tr>
+          <b-th>Type</b-th>
+          <b-th>Member</b-th>
+          <b-th>Writer</b-th>
+          <b-th>Title</b-th>
+          <b-th>Date</b-th>
+        </b-tr>
+      </b-thead>
 
-                            <tr v-for ="value in notices"  @click="detail(value)" >
-                                   <td>{{value.studyType}}</td>
-                                   <td>{{value.maxMember}}</td>
-                                   <td >{{value.writer}}</td>&nbsp;&nbsp;&nbsp;
-                                   <td >{{value.title}}</td>&nbsp;&nbsp;&nbsp;&nbsp;
-                                   <td >{{value.create_date}}</td>
-                            </tr>
-                            <br>
-                     </table>
-                     <button class="mybtn2" @click="prevPage">
-                            Previous
-                     </button>&nbsp;&nbsp;&nbsp;
-                     <button class="mybtn2" @click="nextPage">
-                            Next
-                     </button>
-                     <br>
-                     <br>
-                     <br>
-                     <button type="submit" class = "mybtn" @click="write()"> Study 생성 </button>
-              </div>
-       </div>
+      <b-tbody>
+        <b-tr v-for="value in notices" :key="value.id" @click="detail(value)">
+          <b-td>{{value.studyType}}</b-td>
+          <b-td>{{value.maxMember}}</b-td>
+          <b-td>{{value.writer}}</b-td>
+          <b-td>{{value.title}}</b-td>
+          <b-td>{{value.create_date}}</b-td>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
+    <p><button class="btn mybtn2" @click="prevPage">Previous</button>
+    <button class="btn mybtn2" @click="nextPage">Next</button></p>
+
+    <button type="submit" class="mybtn btn" @click="write()">Study 생성</button>
+  </div>
 </template>
 <script>
-       export default {
-              data:function (){
-                     return {
-                            pageNumber:0,
-                            size :5,
-                     }
-              },
-              computed:{
-                     notices(){
-                            const start = this.pageNumber * this.size,
-                                    end = start + this.size
-                            var temp =this.$store.state.notices
-                            return temp.slice(start,end)
-                     },
+export default {
+  data: function() {
+    return {
+//       fields: ["studyType", "maxMember", "writer", "title", "create_date"],
+      pageNumber: 0,
+      size: 5
+    };
+  },
+  computed: {
+    notices() {
+      const start = this.pageNumber * this.size,
+        end = start + this.size;
+      var temp = this.$store.state.notices;
+      return temp.slice(start, end);
+    }
+  },
+  methods: {
+    write() {
+      this.$router.push({
+        name: "Create"
+      });
+    },
+    detail(index) {
+      this.$router.push({
+        name: "Detail",
+        params: {
+          id: index._id
+        }
+      });
+    },
+    nextPage() {
+      this.pageNumber++;
+    },
+    prevPage() {
+      this.pageNumber--;
+    }
+  },
 
-              },
-              methods:{
-                     write(){
-                            this.$router.push({
-                                   name: 'Create',
-
-                            })
-                     },
-                     detail(index){
-
-                            this.$router.push({
-                                   name :'Detail',
-                                   params:{
-                                          id:index._id
-                                   }
-                            })
-
-                     },
-                     nextPage(){
-                            this.pageNumber++;
-                     },
-                     prevPage(){
-                            this.pageNumber--;
-                     }
-              },
-
-              async beforeCreate() {
-                     await this.$store.dispatch('fetchNotices')
-              }
-       }
+  async beforeCreate() {
+    await this.$store.dispatch("fetchNotices");
+  }
+};
 </script>
 
 <style scoped>
-       .outer{
-              display: table;
-              width: 100%;
-              height: 100%;
-       }
-       .inner{
-              display: table-cell;
-              vertical-align: middle;
-              text-align: center;
-       }
-       .notice{
-              width: 60%;
-              height: 100px;
-              margin: auto;
-              text-align: center;
+.mybtn {
+  border-radius: 10px;
+  background-color: #ecd7d2 !important;
+}
+.mybtn2 {
+  border-radius: 10px;
+  background: rgba(230, 230, 230, 0.517);
+}
 
-       }
-       .mybtn{
-              border-radius:10px;
-              background-color: #ecd7d2 !important;
-       }
-       .mybtn2{
-              border-radius: 10px;
-              background:rgba(230, 230, 230, 0.517);
-       }
-
+table > td {
+  word-break: break-all;
+}
+table {
+  text-align: center;
+}
 </style>
